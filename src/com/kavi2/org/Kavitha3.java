02a3bf9627970d6f91f033f61bf66db5b3c0fec6
Kavv123 <kavitha.vkg.vishnu@gmail.com>
2022-06-29 23:34:04 +0530

    KavithaProjectcreated

---
+++ src/com/kavi2/org/Kavitha3.java
@@ -0,0 +1,14 @@
+package com.kavi2.org;
+
+public class Kavitha3 {
+private void methodA() {
+	// TODO Auto-generated method stub
+System.out.println(" TEST PROGRAM ");
+}
+public static void main(String[] args) {
+	
+	Kavitha3 org = new Kavitha3();
+	org.methodA();
+	
+}
+}
